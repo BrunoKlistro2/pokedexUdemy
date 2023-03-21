dbb962c8217b3b28ea6764aee519782b3e566fec
Bruno Calistro <BrunoCalistro4002@hotmail.com>
2023-03-21 09:37:23 -0300

    alteraçâo do details.ts

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -25,10 +25,10 @@ export class DetailsComponent implements OnInit{
 
 
   ngOnInit(): void{
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon(){
+  public getPokemon(){
     const id = this.activeRoute.snapshot.params['id']
     const pokemon = this.pokeService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeService.apiGetPokemons(`${this.urlName}/${id}`);
